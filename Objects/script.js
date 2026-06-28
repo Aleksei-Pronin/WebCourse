57445206f8494c82f1e30b19c36c3b87f4d64719
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2026-06-28 16:12:45 +0700

    задача Работа с объектами

---
+++ Objects/script.js
@@ -0,0 +1,56 @@
+(() => {
+    const createCountries = () => [
+        {
+            name: "Россия",
+            cities: [
+                { name: "Москва", population: 13000000 },
+                { name: "Санкт-Петербург", population: 5600000 },
+                { name: "Новосибирск", population: 1600000 },
+                { name: "Екатеринбург", population: 1500000 },
+                { name: "Казань", population: 1300000 }
+            ]
+        },
+        {
+            name: "США",
+            cities: [
+                { name: "Нью-Йорк", population: 8400000 },
+                { name: "Лос-Анджелес", population: 3900000 },
+                { name: "Вашингтон", population: 5600000 },
+                { name: "Чикаго", population: 2700000 },
+                { name: "Хьюстон", population: 2300000 }
+            ]
+        },
+        {
+            name: "Китай",
+            cities: [
+                { name: "Пекин", population: 21500000 },
+                { name: "Шанхай", population: 24800000 },
+                { name: "Гуанчжоу", population: 15000000 },
+                { name: "Шэньчжэнь", population: 13000000 }
+            ]
+        },
+        {
+            name: "Франция",
+            cities: [
+                { name: "Париж", population: 2200000 },
+                { name: "Марсель", population: 870000 },
+                { name: "Лион", population: 520000 }
+            ]
+        }
+    ];
+
+    const getCountriesWithMaxCities = (countries) => {
+        const maxCitiesCount = countries.reduce((max, country) => Math.max(max, country.cities.length), 0);
+        return countries.filter(country => country.cities.length === maxCitiesCount);
+    };
+
+    const getPopulationByCountry = (countries) => countries.reduce((result, country) => {
+        result[country.name] = country.cities.reduce((sum, city) => sum + city.population, 0);
+        return result;
+    }, {});
+
+    const countries = createCountries();
+    console.log("Страны:", countries);
+    console.log("Страны с максимальным количеством городов:", getCountriesWithMaxCities(countries));
+    console.log("Cуммарная численность по стране:", getPopulationByCountry(countries));
+})();
