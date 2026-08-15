f38d605374e37d66252b473a77ec8e6e1256f691
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2026-08-15 11:59:04 +0700

    Задача «TodoList»

--- Lodash/script.js
+++ Lodash/script.js
@@ -2,16 +2,16 @@
 
 (function () {
     const people = [
-        {name: 'Анна', age: 25},
-        {name: 'Михаил', age: 32},
-        {name: 'Елена', age: 22},
-        {name: 'Анна', age: 28},
-        {name: 'Дмитрий', age: 19},
-        {name: 'Ольга', age: 30},
-        {name: 'Сергей', age: 26},
-        {name: 'Анна', age: 23},
-        {name: 'Ирина', age: 35},
-        {name: 'Павел', age: 27}
+        {name: "Анна", age: 25},
+        {name: "Михаил", age: 32},
+        {name: "Елена", age: 22},
+        {name: "Анна", age: 28},
+        {name: "Дмитрий", age: 19},
+        {name: "Ольга", age: 30},
+        {name: "Сергей", age: 26},
+        {name: "Анна", age: 23},
+        {name: "Ирина", age: 35},
+        {name: "Павел", age: 27}
     ];
 
     const averageAge = _.meanBy(people, "age");
@@ -23,9 +23,9 @@
         .value();
     console.log("Люди от 20 до 30 лет (по возрастанию): ", filteredAndSortedPeople);
 
-    const uniqueAndSortedNames = _.chain(people)
+    const uniqueNamesSortedDescending = _.chain(people)
         .filter(person => person.age >= 20 && person.age <= 30)
-        .map('name')
+        .map("name")
         .uniq()
         .sortBy()
         .reverse()
