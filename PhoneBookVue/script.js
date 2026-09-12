af1cf79c0a8473924595635d03559ce84e8a76e8
Pronin Aleksei <aleksei.s.pronin@gmail.com>
2026-09-12 15:52:22 +0700

    Задача «PhoneBook Vue»

---
+++ PhoneBookVue/script.js
@@ -0,0 +1,219 @@
+"use strict";
+
+const app = Vue.createApp({
+    data() {
+        return {
+            contacts: [],
+            filter: "",
+            searchText: "",
+            selectAll: false,
+            newContact: {
+                surname: "",
+                name: "",
+                phone: ""
+            },
+            errors: {
+                surname: "",
+                name: "",
+                phone: ""
+            },
+            nextId: 1
+        };
+    },
+
+    computed: {
+        filteredContacts() {
+            const searchText = this.searchText.trim().toLowerCase();
+
+            if (!searchText) {
+                return this.contacts;
+            }
+
+            return this.contacts.filter(contact => {
+                const contactText =
+                    contact.surname + " " +
+                    contact.name + " " +
+                    contact.phone;
+
+                return contactText.toLowerCase().includes(searchText);
+            });
+        },
+
+        selectedContacts() {
+            return this.contacts.filter(contact => contact.selected);
+        }
+    },
+
+    methods: {
+        addContact() {
+            const surname = this.newContact.surname;
+            const name = this.newContact.name;
+            const phone = this.newContact.phone;
+
+            if (!surname) {
+                this.errors.surname = "Заполните поле";
+            }
+
+            if (!name) {
+                this.errors.name = "Заполните поле";
+            }
+
+            if (!phone) {
+                this.errors.phone = "Заполните поле";
+            }
+
+            if (!surname || !name || !phone) {
+                return;
+            }
+
+            if (this.isPhoneAlreadyExists(phone, null)) {
+                this.errors.phone = "Контакт с таким номером уже добавлен";
+                return;
+            }
+
+            this.contacts.push({
+                id: this.nextId++,
+                surname: surname,
+                name: name,
+                phone: phone,
+                selected: false,
+                editing: false,
+                editSurname: "",
+                editName: "",
+                editPhone: "",
+                surnameError: "",
+                nameError: "",
+                phoneError: ""
+            });
+
+            this.newContact.surname = "";
+            this.newContact.name = "";
+            this.newContact.phone = "";
+        },
+
+        isPhoneAlreadyExists(phone, currentContact) {
+            return this.contacts.some(contact => {
+                if (currentContact && contact.id === currentContact.id) {
+                    return false;
+                }
+
+                return contact.phone.trim() === phone;
+            });
+        },
+
+        deleteContact(contact) {
+            Notiflix.Confirm.show(
+                "Удалить контакт?",
+                "Контакт будет удалён из таблицы",
+                "Удалить",
+                "Отмена",
+                () => {
+                    const index = this.contacts.indexOf(contact);
+
+                    if (index !== -1) {
+                        this.contacts.splice(index, 1);
+                    }
+
+                    this.updateSelectAll();
+                }
+            );
+        },
+
+        deleteSelected() {
+            Notiflix.Confirm.show(
+                "Удалить выбранные контакты?",
+                "Количество контактов: " + this.selectedContacts.length,
+                "Удалить",
+                "Отмена",
+                () => {
+                    this.contacts = this.contacts.filter(contact => !contact.selected);
+                    this.selectAll = false;
+                }
+            );
+        },
+
+        selectAllContacts() {
+            this.contacts.forEach(contact => {
+                contact.selected = this.selectAll;
+            });
+        },
+
+        updateSelectAll() {
+            if (this.contacts.length === 0) {
+                this.selectAll = false;
+                return;
+            }
+
+            this.selectAll = this.contacts.every(contact => contact.selected);
+        },
+
+        editContact(contact) {
+            contact.editSurname = contact.surname;
+            contact.editName = contact.name;
+            contact.editPhone = contact.phone;
+            contact.editing = true;
+        },
+
+        saveContact(contact) {
+            contact.surnameError = "";
+            contact.nameError = "";
+            contact.phoneError = "";
+
+            const surname = contact.editSurname;
+            const name = contact.editName;
+            const phone = contact.editPhone;
+
+            if (!surname) {
+                contact.surnameError = "Заполните поле";
+            }
+
+            if (!name) {
+                contact.nameError = "Заполните поле";
+            }
+
+            if (!phone) {
+                contact.phoneError = "Заполните поле";
+            }
+
+            if (!surname || !name || !phone) {
+                return;
+            }
+
+            if (this.isPhoneAlreadyExists(phone, contact)) {
+                contact.phoneError = "Контакт с таким номером уже добавлен";
+                return;
+            }
+
+            contact.surname = surname;
+            contact.name = name;
+            contact.phone = phone;
+            contact.editing = false;
+        },
+
+        cancelEdit(contact) {
+            contact.surnameError = "";
+            contact.nameError = "";
+            contact.phoneError = "";
+            contact.editing = false;
+        },
+
+        filterContacts() {
+            this.searchText = this.filter;
+        },
+
+        clearFilter() {
+            this.filter = "";
+            this.searchText = "";
+        }
+    }
+});
+
+app.mount("#app");
+
+Notiflix.Confirm.init({
+    borderRadius: "0.375rem",
+    titleColor: "#000",
+    titleFontSize: "20px",
+    okButtonBackground: "#0d6efd",
+    cancelButtonBackground: "#6c757d"
+});
